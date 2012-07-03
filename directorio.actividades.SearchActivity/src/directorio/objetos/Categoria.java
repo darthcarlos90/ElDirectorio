f1521d8a111a7af7b621b1591c71cb84238a1153
darthcarlos90 <darth_carlos_90@hotmail.com>
2012-07-02 21:56:19 -0500

    Agregue otra clase categoria -.-

---
+++ directorio.actividades.SearchActivity/src/directorio/objetos/Categoria.java
@@ -0,0 +1,31 @@
+package directorio.objetos;
+
+public class Categoria {
+	private int id;
+	private String nombre;
+	private char letra;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public char getLetra() {
+		return letra;
+	}
+
+	public void setLetra(char letra) {
+		this.letra = letra;
+	}
+}
