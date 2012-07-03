c85ce5541cf08c3254b06a17c1bda39b6bd60984
darthcarlos90 <darth_carlos_90@hotmail.com>
2012-07-02 22:13:22 -0500

    Creación del paquete Dao

---
+++ directorio.actividades.SearchActivity/src/directorio/DAO/AdvertiserDAO.java
@@ -0,0 +1,12 @@
+package directorio.DAO;
+
+/**
+ * Clase para accesar a la tabla de Advertiser y otros datos que tengan que ver
+ * con advertiser.
+ * 
+ * @author Carlos Tirado
+ * 
+ */
+public class AdvertiserDAO {
+
+}
