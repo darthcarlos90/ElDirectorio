0d7811841b0204baa461c6a4592e0569b5132abb
darthcarlos90 <darth_carlos_90@hotmail.com>
2012-07-05 21:26:45 -0500

    Se creo la clase categoria dao, que accesa a la base de datos para traer categorias. el boton de mostar categorias ya funciona.

---
+++ directorio.actividades.SearchActivity/src/directorio/DAO/CategoriaDAO.java
@@ -0,0 +1,83 @@
+package directorio.DAO;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import directorio.objetos.Categoria;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+
+public class CategoriaDAO {
+	private SQLiteDatabase database;
+	private ArrayList<String> categorias;
+	private ArrayList<Categoria> cats;
+	private File file;
+	private static final String LOCATION_DB = Environment
+			.getExternalStorageDirectory().getPath() + "/DirLaguna.db";
+
+	public CategoriaDAO() {
+		file = new File(LOCATION_DB);
+		database = SQLiteDatabase.openOrCreateDatabase(file, null);
+
+	}
+
+	/**
+	 * Método que carga las categorias de la base de datos.
+	 * 
+	 * @return El ArrayList con las categorias de la base de datos.
+	 */
+	public ArrayList<String> getCategorias() {
+		cargaCategorias();
+		categorias = new ArrayList<String>();
+		for (int i = 0; i < cats.size(); i++) {
+			categorias.add(cats.get(i).getNombre());
+		}
+		return categorias;
+	}
+
+	public ArrayList<Categoria> getCats() {
+		return cats;
+	}
+
+	public void setCats(ArrayList<Categoria> cats) {
+		this.cats = cats;
+	}
+
+	public void setCategorias(ArrayList<String> categorias) {
+		this.categorias = categorias;
+	}
+
+	/**
+	 * Método que hace el query a la base de datos y guarda las categorias en un
+	 * ArrayList.
+	 */
+	private void cargaCategorias() {
+
+		cats = new ArrayList<Categoria>();
+		Cursor cursor = database.rawQuery("SELECT * FROM Category", null);
+		cursor.moveToFirst();
+		if (!cursor.isAfterLast()) {
+			do {
+				try {
+
+					int id = Integer.parseInt(cursor.getString(cursor
+							.getColumnIndex("CategoryId")));
+					String nombre = cursor.getString(cursor
+							.getColumnIndex("CatName"));
+					char letra = cursor.getString(
+							cursor.getColumnIndex("Letter")).charAt(0);
+					Categoria categoria = new Categoria(id, nombre, letra);
+					cats.add(categoria);
+				} catch (NullPointerException e) {
+					System.err
+							.println("One of the database fields doesn't exist");
+				}
+			} while (cursor.moveToNext());
+		}
+
+		cursor.close();
+
+	}
+}
--- directorio.actividades.SearchActivity/src/directorio/actividades/MostrarCategoriasActivity.java
+++ directorio.actividades.SearchActivity/src/directorio/actividades/MostrarCategoriasActivity.java
@@ -1,7 +1,9 @@
 package directorio.actividades;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
+import directorio.DAO.CategoriaDAO;
 import directorio.objetos.Categoria;
 
 import android.app.Activity;
@@ -12,48 +14,25 @@ import android.widget.ListView;
 public class MostrarCategoriasActivity extends Activity {
 
 	ListView lv;
-	ArrayList<Categoria> datosTentativos = new ArrayList<Categoria>();
-	String[] datosTentativos2;
-	Categoria cat1;
-	Categoria cat2;
-	Categoria cat3;
-	Categoria cat4;
+	Categoria categoria;
+	CategoriaDAO catDao;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.categorias);
 		setupViews();
 	}
-	
 
 	public void setupViews() {
 		lv = (ListView) findViewById(R.id.mostrar_categorias);
-		cat1 = new Categoria(1, "categoria 1", 'A');
-		cat2 = new Categoria(2, "categoria 2", 'B');
-		cat3 = new Categoria(3, "categoria 3", 'C');
-		cat4 = new Categoria(4, "Pinche error te la mamaste cabron", 'D');
-		datosTentativos.add(cat1);
-		datosTentativos.add(cat2);
-		datosTentativos.add(cat3);
-		datosTentativos.add(cat4);
-		ArrayList<String> lista = new ArrayList<String>();
-
-		char letra1 = datosTentativos.get(0).getLetra();
-		lista.add(letra1 + "");
-		for (int i = 0; i < datosTentativos.size(); i++) {
-			char temp = datosTentativos.get(i).getLetra();
-			if (temp != letra1) {
-				letra1 = temp;
-				lista.add(letra1 + "");
-			}
-			lista.add(datosTentativos.get(i).getNombre());
-
-		}
-		datosTentativos2 = new String[lista.size()];
+		catDao = new CategoriaDAO();
+		ArrayList<String> lista = catDao.getCategorias();
+		Collections.sort(lista, String.CASE_INSENSITIVE_ORDER);
+		String[] datosTentativos2 = new String[lista.size()];
 		for (int i = 0; i < lista.size(); i++) {
 			datosTentativos2[i] = lista.get(i);
 		}
-		
+
 		lv.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item,
 				datosTentativos2));
 	}
